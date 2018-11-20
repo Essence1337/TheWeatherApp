779e08f9e9f81f2b484a168aa0d5ecfec3e1ec8e
Timur Koshevoy <timurkoshevoy@gmail.com>
2018-11-20 22:54:13 +0200

    minor

--- TheWeatherApp/SelectCityViewController.swift
+++ TheWeatherApp/SelectCityViewController.swift
@@ -9,59 +9,59 @@
 import UIKit
 import SwiftyJSON
 
-class ViewController: UIViewController,UITableViewDelegate, UITableViewDataSource{
-
+class SelectCityViewController: UIViewController {
     
-    var tableView = UITableView()
+    @IBOutlet weak var citiesTableView: UITableView!
+    
     var dataArray: JSON = []
+    let identifire = "customCell"
     
+    //MARK: - Functions
     override func loadView() {
         super.loadView()
-        
+        getJson()
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = UIColor.black
-        
-        self.tableView = UITableView(frame: view.bounds, style: .plain)
-        self.tableView.register(UITableViewCell.self, forCellReuseIdentifier: "custoCell")
-        self.tableView.delegate = self
-        self.tableView.dataSource = self
-        view.addSubview(self.tableView)
+    }
+    
+    func getJson() {
         let path = Bundle.main.path(forResource: "cities", ofType: "json")
         let data = NSData(contentsOfFile: path!)
-        if let jsonData = data {
-            do{
-                let jsonData = try JSON(data: Data(referencing: data!))
-                dataArray = jsonData
-            }catch is Error {}
-            
+        do{
+            let jsonData = try JSON(data: Data(referencing: data!))
+            dataArray = jsonData
+        }catch {
+            print("Error")
         }
     }
+    
+}
 
+//MARK: - Extensions
+extension SelectCityViewController: UITableViewDelegate, UITableViewDataSource {
+    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return dataArray.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "custoCell", for: indexPath)
-//        let cell:UITableViewCell = UITableViewCell()
+        let cell = tableView.dequeueReusableCell(withIdentifier: identifire, for: indexPath)
         if let str = dataArray[indexPath.row]["name"].rawString(){
             cell.textLabel?.text = str
         }
-        
         return cell
     }
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        if let str = dataArray[indexPath.row]["lat"].rawString(){
-            print("indexpath:\(str)")
-//            locatio(vavalfas d sadhsad sa)
+        if let latStr = dataArray[indexPath.row]["lat"].rawString(){
+            print("indexpath:\(latStr)")
+            //            locatio(vavalfas d sadhsad sa)
         }
-        
+        if let lonStr = dataArray[indexPath.row]["lon"].rawString(){
+            print("indexpath:\(lonStr)")
+            //            locatio(vavalfas d sadhsad sa)
+        }
     }
-    
-    
-    
 }
